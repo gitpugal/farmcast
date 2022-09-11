b3a20d518854f716c7e3632e844bcc5e023ae0dc
unknown <pugalarasan2014@gmail.com>
2022-09-11 13:27:08 +0530

    4 deploy

--- app.js
+++ app.js
@@ -115,4 +115,6 @@ app.post("/", function(req, res) {
 
 })
 
-app.listen(process.env.PORT || 3000, console.log("Server is running at port 3000........"));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is running at port 3000........");
+});
